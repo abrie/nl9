5eb2762903f93db7c175b82cd9e130e75769215f
abrie <abrie@hello>
2024-10-31 14:07:21 -0400

    Use createLayer instead of createDynamicLayer.

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,45 +1,52 @@
-import Phaser from 'phaser';
-import { generateSolidColorTexture } from '../utils/TextureGenerator';
+import Phaser from "phaser";
+import { generateSolidColorTexture } from "../utils/TextureGenerator";
 
 class PlayScene extends Phaser.Scene {
-  private tilemap: Phaser.Tilemaps.Tilemap;
-  private tileset: Phaser.Tilemaps.Tileset;
-  private layer: Phaser.Tilemaps.DynamicTilemapLayer;
+	private tilemap: Phaser.Tilemaps.Tilemap;
+	private tileset: Phaser.Tilemaps.Tileset;
+	private layer: Phaser.Tilemaps.DynamicTilemapLayer;
 
-  constructor() {
-    super({ key: 'PlayScene' });
-  }
+	constructor() {
+		super({ key: "PlayScene" });
+	}
 
-  preload() {
-    generateSolidColorTexture(this, 'unfilled', 0xff0000, 32, 32);
-    generateSolidColorTexture(this, 'filled', 0x00ff00, 32, 32);
-  }
+	preload() {
+		generateSolidColorTexture(this, "unfilled", 0xff0000, 32, 32);
+		generateSolidColorTexture(this, "filled", 0x00ff00, 32, 32);
+	}
 
-  create() {
-    this.tilemap = this.make.tilemap({ width: 25, height: 19, tileWidth: 32, tileHeight: 32 });
-    this.tileset = this.tilemap.addTilesetImage('unfilled');
-    this.layer = this.tilemap.createDynamicLayer(0, this.tileset, 0, 0);
+	create() {
+		this.tilemap = this.make.tilemap({
+			data: [],
+			width: 25,
+			height: 19,
+			tileWidth: 32,
+			tileHeight: 32,
+		});
 
-    for (let y = 0; y < this.tilemap.height; y++) {
-      for (let x = 0; x < this.tilemap.width; x++) {
-        this.layer.putTileAt(0, x, y);
-      }
-    }
+		const unfilledTileset = this.tilemap.addTilesetImage("unfilled");
+		this.layer = this.tilemap.createLayer(1, this.tileset, 0, 0);
+		for (let y = 0; y < this.tilemap.height; y++) {
+			for (let x = 0; x < this.tilemap.width; x++) {
+				this.layer.putTileAt(unfilledTileset?.firstgid, x, y);
+			}
+		}
 
-    // Randomly place filled tiles
-    const filledTileset = this.tilemap.addTilesetImage('filled');
-    for (let y = 0; y < this.tilemap.height; y++) {
-      for (let x = 0; x < this.tilemap.width; x++) {
-        if (Math.random() < 0.2) { // 20% chance to place a filled tile
-          this.layer.putTileAt(filledTileset.firstgid, x, y);
-        }
-      }
-    }
-  }
+		// Randomly place filled tiles
+		const filledTileset = this.tilemap.addTilesetImage("filled");
+		for (let y = 0; y < this.tilemap.height; y++) {
+			for (let x = 0; x < this.tilemap.width; x++) {
+				if (Math.random() < 0.2) {
+					// 20% chance to place a filled tile
+					this.layer.putTileAt(filledTileset?.firstgid, x, y);
+				}
+			}
+		}
+	}
 
-  update() {
-    // Update game objects here
-  }
+	update() {
+		// Update game objects here
+	}
 }
 
 export default PlayScene;
