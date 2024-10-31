bf35ad29cdcbe0c5c15480351e46dc5114e2d8af
abrie <abrie@hello>
2024-10-31 15:09:27 -0400

    Correctly referencing the multiple tilesets!

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -31,8 +31,25 @@ class PlayScene extends Phaser.Scene {
 			tileHeight: 32,
 		});
 
-		const unfilledTileset = this.tilemap.addTilesetImage("unfilled", undefined, 32, 32, 0, 0, 1);
-		const filledTileset = this.tilemap.addTilesetImage("filled", undefined, 32, 32, 0, 0, 2);
+		const unfilledTileset = this.tilemap.addTilesetImage(
+			"unfilled",
+			undefined,
+			32,
+			32,
+			0,
+			0,
+			1,
+		);
+		this.tilemap.addTilesetImage;
+		const filledTileset = this.tilemap.addTilesetImage(
+			"filled",
+			undefined,
+			32,
+			32,
+			0,
+			0,
+			2,
+		);
 
 		this.tilemap.tilesets.forEach((tileset) => console.log(tileset));
 		if (unfilledTileset === null || filledTileset === null) {
@@ -46,7 +63,13 @@ class PlayScene extends Phaser.Scene {
 		);
 		for (let y = 0; y < this.tilemap.height; y++) {
 			for (let x = 0; x < this.tilemap.width; x++) {
-				this.layer.putTileAt(unfilledTileset.firstgid, x, y);
+				this.layer.putTileAt(
+					Math.random() > 0.5
+						? filledTileset.firstgid
+						: unfilledTileset.firstgid,
+					x,
+					y,
+				);
 			}
 		}
 	}
