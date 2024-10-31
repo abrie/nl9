10b87cba8c92271b6d009c187063fa3c249f75af
abrie <abrie@users.noreply.github.com>
2024-10-30 23:04:08 -0400

    Modify `generateSolidColorTexture` function to accept a `scene` parameter
    * Change function signature to include `scene` parameter
    * Update `graphics` object creation to use the provided `scene` parameter

--- src/utils/TextureGenerator.ts
+++ src/utils/TextureGenerator.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 
-export function generateSolidColorTexture(name: string, color: number, width: number, height: number): string {
-  const graphics = new Phaser.GameObjects.Graphics(null);
+export function generateSolidColorTexture(scene: Phaser.Scene, name: string, color: number, width: number, height: number): string {
+  const graphics = new Phaser.GameObjects.Graphics(scene);
   graphics.fillStyle(color, 1);
   graphics.fillRect(0, 0, width, height);
 
