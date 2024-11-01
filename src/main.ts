12d1df226135ed4ef06eb6bc27a4969b68f543be
abrie <abrie@users.noreply.github.com>
2024-10-31 22:10:26 -0400

    Enable 2d physics in the Phaser game configuration
    * Add `physics` property to `config` object with default set to 'arcade'
    * Set gravity to affect the player with a y value of 300
    * Disable debug mode for arcade physics

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ const config: Phaser.Types.Core.GameConfig = {
   width: 800,
   height: 600,
   scene: [PlayScene],
-  parent: 'game-container'
+  parent: 'game-container',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  }
 };
 
 const game = new Phaser.Game(config);
