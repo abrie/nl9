9b84332f382836e18dc48b60bdb75c5d6d885bfa
abrie <abrie@users.noreply.github.com>
2024-11-04 19:54:28 -0500

    Remove mode functionality
    Related to #44
    
    Remove mode functionality from the game.
    
    * Remove the `toggleMovementMode` function and its call in the `update` method in `src/scenes/PlayScene.ts`.
    * Remove the `movementMode` variable and its associated logic in `src/scenes/PlayScene.ts`.
    * Remove the `modeText` object and its updates in the `create` and `updateHud` methods in `src/scenes/PlayScene.ts`.
    * Remove the `z` key input check in the `updateInputs` method in `src/utils/InputManager.ts`.
    * Update the `inputs` and `keys` objects in `src/utils/InputManager.ts` to remove the `z` key.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/abrie/nl9/issues/44?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/utils/InputManager.ts
+++ src/utils/InputManager.ts
@@ -2,12 +2,12 @@ import Phaser from "phaser";
 
 class InputManager {
   private scene: Phaser.Scene;
-  public inputs: { up: boolean; down: boolean; left: boolean; right: boolean; z: boolean; x: boolean; c: boolean; shift: boolean };
-  private keys: { up: Phaser.Input.Keyboard.Key; down: Phaser.Input.Keyboard.Key; left: Phaser.Input.Keyboard.Key; right: Phaser.Input.Keyboard.Key; z: Phaser.Input.Keyboard.Key; x: Phaser.Input.Keyboard.Key; c: Phaser.Input.Keyboard.Key; shift: Phaser.Input.Keyboard.Key };
+  public inputs: { up: boolean; down: boolean; left: boolean; right: boolean; x: boolean; c: boolean; shift: boolean };
+  private keys: { up: Phaser.Input.Keyboard.Key; down: Phaser.Input.Keyboard.Key; left: Phaser.Input.Keyboard.Key; right: Phaser.Input.Keyboard.Key; x: Phaser.Input.Keyboard.Key; c: Phaser.Input.Keyboard.Key; shift: Phaser.Input.Keyboard.Key };
 
   constructor(scene: Phaser.Scene) {
     this.scene = scene;
-    this.inputs = { up: false, down: false, left: false, right: false, z: false, x: false, c: false, shift: false };
+    this.inputs = { up: false, down: false, left: false, right: false, x: false, c: false, shift: false };
 
     if (!this.scene.input.keyboard) {
       throw new Error("Keyboard input is not available.");
@@ -19,7 +19,6 @@ class InputManager {
       down: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN),
       left: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
       right: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
-      z: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z),
       x: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X),
       c: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C),
       shift: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT)
@@ -31,7 +30,6 @@ class InputManager {
     this.inputs.down = this.keys.down.isDown;
     this.inputs.left = this.keys.left.isDown;
     this.inputs.right = this.keys.right.isDown;
-    this.inputs.z = this.keys.z.isDown;
     this.inputs.x = this.keys.x.isDown;
     this.inputs.c = this.keys.c.isDown;
     this.inputs.shift = this.keys.shift.isDown;
